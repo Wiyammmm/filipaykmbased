6047b07b2e60077d1e35d07a8970b3818f40f75b
Wiyammmm <johnwilliamalon-alon@seapps-inc.com>
2024-04-01 14:05:02 +0800

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,25 +7,32 @@ import 'pages/splash.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+    apiKey: 'AIzaSyDiQYQqP5q3gSpyT1TZnP1jzMdwkC2FF1I',
+    appId: '1:890512655620:android:88de65c3cf5538dfbdbc51',
+    messagingSenderId: '1422413396640022852',
+    projectId: 'filipay-7460a',
+    storageBucket: 'filipay-7460a.appspot.com',
+  ));
   await Hive.initFlutter();
 
   final _myBox = await Hive.openBox('myBox');
   initializeData();
-  firebaseRDB();
+  // firebaseRDB();
   runApp(const MyApp());
 }
 
-void firebaseRDB() {
-  print('firebaseRDB()');
-  FirebaseDatabase.instance
-      .ref()
-      .child('filipayqr')
-      .onChildChanged
-      .listen((event) {
-    print('Message changed: ${event.snapshot.value}');
-  });
-}
+// void firebaseRDB() {
+//   print('firebaseRDB()');
+//   FirebaseDatabase.instance
+//       .ref()
+//       .child('filipayqr')
+//       .onChildChanged
+//       .listen((event) {
+//     print('Message changed: ${event.snapshot.value}');
+//   });
+// }
 
 Future<void> initializeData() async {
   final _myBox = await Hive.box('myBox');
